b5a9250aa2f8e01cd148d77e184e7bec23ce17ce
sriutaminingsih <49768122+tamiutami@users.noreply.github.com>
2020-11-05 03:00:01 +0700

    Add files via upload

---
+++ PBOIF2_10116378_LATIHAN8_Pecahan/src/pboif2_10116378_latihan8_pecahan/PBOIF2_10116378_LATIHAN8_Pecahan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10116378_latihan8_pecahan;
+
+/**
+ *
+ * @author Sri Utami Ningsih
+ * Nama  : Sri Utami Ningsih
+ * Kelas : PBO-IF2
+ * Nim   : 10116378
+ */
+public class PBOIF2_10116378_LATIHAN8_Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float  : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
